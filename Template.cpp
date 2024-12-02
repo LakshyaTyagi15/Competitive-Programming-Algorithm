bc77f6b7e23ab16cb3152c79717ccae5a1ffd380
LakshyaTyagi15 <86817577+LakshyaTyagi15@users.noreply.github.com>
2024-12-02 19:06:16 +0530

    My template

---
+++ Template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+ll mod = 1e9 + 7;
+ll modd = 998244353;
+
+void solve() {
+  
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    t = 1;
+    while (t--) solve();
+
+    return 0;
+}
