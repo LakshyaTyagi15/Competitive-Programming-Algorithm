afe14ab81f9a98e50f6d1d177c7053f1d6d2c4aa
LakshyaTyagi15 <86817577+LakshyaTyagi15@users.noreply.github.com>
2025-01-08 16:57:58 +0530

    Segment_Tree.cpp

--- Segment_Tree.cpp
+++ Segment_Tree.cpp
@@ -7,8 +7,10 @@ using ll = long long;
 class SegmentTree
 {
 public:
+    ll n;
+    vector<ll> segtree, lazy;
 
-    void buildSegmentTree(ll i, ll l, ll r, vector<ll> &segtree, vector<ll> &arr)
+    void buildSegmentTree(ll i, ll l, ll r, vector<ll> &arr)
     {
         if (l == r)
         {
@@ -16,13 +18,25 @@ public:
             return;
         }
         ll mid = l + ((r - l) >> 1LL);
-        buildSegmentTree(2LL * i + 1LL, l, mid, segtree, arr);
-        buildSegmentTree(2LL * i + 2LL, mid + 1LL, r, segtree, arr);
+        buildSegmentTree(2LL * i + 1LL, l, mid, arr);
+        buildSegmentTree(2LL * i + 2LL, mid + 1LL, r, arr);
         segtree[i] = segtree[2LL * i + 1LL] + segtree[2LL * i + 2LL];
     }
 
-    ll querySegmentTree(ll start, ll end, ll i, ll l, ll r, vector<ll> &segtree)
+    ll querySegmentTree(ll start, ll end, ll i, ll l, ll r)
     {
+
+        if (lazy[i] != 0)
+        {
+            segtree[i] += (r - l + 1LL) * lazy[i];
+            if (l != r)
+            {
+                lazy[2LL * i + 1LL] += lazy[i];
+                lazy[2LL * i + 2LL] += lazy[i];
+            }
+            lazy[i] = 0;
+        }
+
         if (l > end || r < start)
         {
             return 0;
@@ -33,11 +47,21 @@ public:
         }
 
         ll mid = l + ((r - l) >> 1LL);
-        return querySegmentTree(start, end, 2LL * i + 1LL, l, mid, segtree) + querySegmentTree(start, end, 2LL * i + 2LL, mid + 1LL, r, segtree);
+        return querySegmentTree(start, end, 2LL * i + 1LL, l, mid) + querySegmentTree(start, end, 2LL * i + 2LL, mid + 1LL, r);
     }
 
-    void updateSegmentTree(ll idx, ll i, ll l, ll r, ll val, vector<ll> &segtree)
+    void updateSegmentTree(ll idx, ll i, ll l, ll r, ll val)
     {
+        if (lazy[i] != 0)
+        {
+            segtree[i] += (r - l + 1) * lazy[i];
+            if (l != r)
+            {
+                lazy[2LL * i + 1LL] += lazy[i];
+                lazy[2LL * i + 2LL] += lazy[i];
+            }
+            lazy[i] = 0;
+        }
         if (l == r)
         {
             segtree[i] = val;
@@ -46,24 +70,73 @@ public:
 
         ll mid = l + ((r - l) >> 1LL);
         if (idx <= mid)
-            updateSegmentTree(idx, 2 * i + 1, l, mid, val, segtree);
+            updateSegmentTree(idx, 2 * i + 1, l, mid, val);
         else
-            updateSegmentTree(idx, 2LL * i + 2LL, mid + 1LL, r, val, segtree);
+            updateSegmentTree(idx, 2LL * i + 2LL, mid + 1LL, r, val);
         segtree[i] = segtree[2LL * i + 1LL] + segtree[2LL * i + 2LL];
     }
 
-    void build(vector<ll>&a, ll n) {
-        vector<ll> segtree(4LL * n, -1);
-        buildSegmentTree(0, 0, n - 1, segtree, a);
+    void LazyPropagation(ll s, ll e, ll i, ll l, ll r, ll val)
+    {
+        if (lazy[i] != 0)
+        {
+            segtree[i] += (r - l + 1LL) * lazy[i];
+            if (l != r)
+            {
+                lazy[2LL * i + 1LL] += lazy[i];
+                lazy[2LL * i + 2LL] += lazy[i];
+            }
+            lazy[i] = 0;
+        }
+        if (s > r || e < l || l > r)
+        {
+            return;
+        }
+
+        if (l >= s && r <= e)
+        {
+            segtree[i] += (r - l + 1LL) * val;
+            if (l != r)
+            {
+                lazy[2LL * i + 1LL] += val;
+                lazy[2LL * i + 2LL] += val;
+            }
+            return;
+        }
+
+        ll mid = l + ((r - l) >> 1LL);
+        LazyPropagation(s, e, 2LL * i + 1LL, l, mid, val);
+        LazyPropagation(s, e, 2LL * i + 2LL, mid + 1, r, val);
+        segtree[i] = segtree[2LL * i + 1LL] + segtree[2LL * i + 2LL];
+    }
+
+    SegmentTree(vector<ll> &a)
+    {
+        n = a.size();
+        segtree.resize(4LL * n, 0);
+        lazy.resize(4LL * n, 0);
+        buildSegmentTree(0, 0, n - 1LL, a);
+    }
+
+    void update(ll idx, ll val)
+    {
+        updateSegmentTree(idx, 0, 0, n - 1LL, val);
+    }
+    void update(ll s, ll e, ll val)
+    {
+        LazyPropagation(s, e, 0, 0, n - 1LL, val);
+    }
+
+    ll query(ll s, ll e)
+    {
+        return querySegmentTree(s, e, 0, 0, n - 1LL);
     }
 };
 
 int main()
 {
-    vector<ll>arr = {1, 2, 3, 4, 5, 6, 7, 8};
-    SegmentTree seg;
-    seg.build(arr, 8);
-
+    vector<ll> arr = {1, 2, 3, 4, 5, 6, 7, 8};
+    SegmentTree seg(arr);
 
     return 0;
 }
