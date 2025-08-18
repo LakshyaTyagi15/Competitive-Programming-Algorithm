80c07adf7258fefaee705286fc0e01eb3d1f51a2
LakshyaTyagi15 <86817577+LakshyaTyagi15@users.noreply.github.com>
2025-08-18 17:39:25 +0530

    Dijkstra - O(v^2).cpp

--- Dijkstra - O(v^2).cpp
+++ Dijkstra - O(v^2).cpp
@@ -1,20 +1,32 @@
-#include <iostream>
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define dbg(x...)
+#endif
+
 using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 using ll = long long;
-ll mod = 1e9 + 7;
-ll modd = 998244353;
 
-vector<ll> dijkstra(ll v, vector<vector<ll>> adj[], ll s)
+template <typename T>
+vector<T> dijkstra(ll v, vector<vector<ll>> adj[], ll s)
 {
     vector<bool> explored(v + 1, 0);
-    vector<ll> dist(v + 1, 1e18);
+    vector<T> dist(v + 1, numeric_limits<T>::max());
     dist[s] = 0;
     ll count = v;
     while (count--)
     {
-        ll node = -1, value = 1e18;
+        ll node = -1;
+        T value = 1e18;
         for (ll i = 1; i <= v; i++)
         {
             if (!explored[i] && value > dist[i])
@@ -23,12 +35,12 @@ vector<ll> dijkstra(ll v, vector<vector<ll>> adj[], ll s)
                 value = dist[i];
             }
         }
-        if (node == -1) break;
+
         explored[node] = 1;
         for (ll j = 0; j < adj[node].size(); j++)
         {
             ll neighbour = adj[node][j][0];
-            ll weight = adj[node][j][1];
+            T weight = adj[node][j][1];
             if (!explored[neighbour] && dist[node] + weight < dist[neighbour])
             {
                 dist[neighbour] = dist[node] + weight;
