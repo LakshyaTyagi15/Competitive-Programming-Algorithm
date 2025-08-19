9c4d3acebd8c6a4165af70afb12685b0399df8dd
LakshyaTyagi15 <86817577+LakshyaTyagi15@users.noreply.github.com>
2025-08-19 10:24:54 +0530

    Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,13 +1,24 @@
-#include <iostream>
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#ifdef LOCAL
+#include <debug.h>
+#else
+#define dbg(x...)
+#endif
+
 using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 using ll = long long;
-const ll mod = 1e9 + 7;
-const ll modd = 998244353;
 
-void solve() {
-  
+
+void solve() {    
+    
 }
 
 int main()
@@ -18,6 +29,7 @@ int main()
 
     int t = 1;
     cin >> t;
+    
     while (t--) solve();
 
     return 0;
