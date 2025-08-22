aa317a06e7e8c8b79be8e812552cde82bf6b3672
LakshyaTyagi15 <86817577+LakshyaTyagi15@users.noreply.github.com>
2025-08-22 21:37:48 +0530

    Modular.cpp

--- Modular.cpp
+++ Modular.cpp
@@ -15,6 +15,8 @@ template <typename T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 using ll = long long;
+const ll mod = 1e9 + 7;
+const ll modd = 998244353;
 
 template <typename T, T mod>
 struct Modular
@@ -27,12 +29,10 @@ struct Modular
         if (x < 0)
             x += mod;
     }
-
     constexpr T val()
     {
-        return x = 0;
+        return x;
     }
-
     constexpr Modular &operator++()
     {
         x++;
@@ -40,7 +40,6 @@ struct Modular
             x = 0;
         return *this;
     }
-
     constexpr Modular &operator--()
     {
         if (x == 0)
@@ -48,21 +47,18 @@ struct Modular
         x--;
         return *this;
     }
-
     constexpr Modular operator++(int)
     {
         Modular res = *this;
         ++*this;
         return res;
     }
-
     constexpr Modular operator--(int)
     {
         Modular res = *this;
         --*this;
         return res;
     }
-
     constexpr Modular &operator+=(const Modular &b)
     {
         x += b.x;
@@ -70,7 +66,6 @@ struct Modular
             x -= mod;
         return *this;
     }
-
     constexpr Modular &operator-=(const Modular &b)
     {
         x -= b.x;
@@ -78,7 +73,6 @@ struct Modular
             x += mod;
         return *this;
     }
-
     constexpr Modular &operator*=(const Modular &b)
     {
         long long z = x;
@@ -89,12 +83,10 @@ struct Modular
     }
     constexpr Modular operator+() const { return *this; }
     constexpr Modular operator-() const { return Modular() - *this; }
-
     constexpr Modular &operator/=(const Modular &b)
     {
         return *this = *this * b.inv();
     }
-
     constexpr Modular power(T n) const
     {
         Modular y = *this, r = 1;
@@ -116,17 +108,35 @@ struct Modular
         return power(mod - 2);
     }
 
-    friend constexpr Modular operator+(const Modular &a, T &b) { return Modular(a) += b; }
-    friend constexpr Modular operator-(const Modular &a, T &b) { return Modular(a) -= b; }
-    friend constexpr Modular operator*(const Modular &a, T &b) { return Modular(a) *= b; }
-    friend constexpr Modular operator/(const Modular &a, T &b) { return Modular(a) /= b; }
-    friend constexpr bool operator==(const Modular &a, T &b) { return a.x == b.x; }
-    friend constexpr bool operator!=(const Modular &a, T &b) { return a.x != b.x; }
+    friend constexpr Modular operator+(const Modular &a, const Modular &b) { return Modular(a) += b; }
+    friend constexpr Modular operator-(const Modular &a, const Modular &b) { return Modular(a) -= b; }
+    friend constexpr Modular operator*(const Modular &a, const Modular &b) { return Modular(a) *= b; }
+    friend constexpr Modular operator/(const Modular &a, const Modular &b) { return Modular(a) /= b; }
 
-    Modular power(Modular a, T n)
-    {
-        return a.power(n);
-    }
+    template <typename U>
+    friend constexpr Modular operator+(const Modular &a, const U &b) { return Modular(a) += Modular(b); }
+    template <typename U>
+    friend constexpr Modular operator-(const Modular &a, const U &b) { return Modular(a) -= Modular(b); }
+    template <typename U>
+    friend constexpr Modular operator*(const Modular &a, const U &b) { return Modular(a) *= Modular(b); }
+    template <typename U>
+    friend constexpr Modular operator/(const Modular &a, const U &b) { return Modular(a) /= Modular(b); }
+
+    template <typename U>
+    friend constexpr Modular operator+(const U &a, const Modular &b) { return Modular(a) += b; }
+    template <typename U>
+    friend constexpr Modular operator-(const U &a, const Modular &b) { return Modular(a) -= b; }
+    template <typename U>
+    friend constexpr Modular operator*(const U &a, const Modular &b) { return Modular(a) *= b; }
+    template <typename U>
+    friend constexpr Modular operator/(const U &a, const Modular &b) { return Modular(a) /= b; }
+
+    friend constexpr bool operator==(const Modular &a, const Modular &b) { return a.x == b.x; }
+    friend constexpr bool operator!=(const Modular &a, const Modular &b) { return a.x != b.x; }
+    template <typename U>
+    friend constexpr bool operator==(const Modular &a, const U &b) { return a.x == Modular(b).x; }
+    template <typename U>
+    friend constexpr bool operator!=(const Modular &a, const U &b) { return a.x != Modular(b).x; }
 
     friend istream &operator>>(istream &is, Modular &m)
     {
@@ -135,7 +145,6 @@ struct Modular
         m = Modular(y);
         return is;
     }
-
     friend ostream &operator<<(ostream &os, const Modular &m)
     {
         os << m.x;
@@ -147,33 +156,47 @@ struct Modular
     }
     constexpr operator T() const { return x; }
 };
-using mll = Modular<ll, ll(1e9 + 7)>;
-vector<mll> fact;
 
-void f(ll n = 1e6 + 10)
+using mll = Modular<ll, mod>;
+vector<mll> fact, invfact;
+
+mll modInverse(mll a)
+{
+    return a.inv();
+}
+
+void f(ll n = 2e5 + 10)
 {
     fact.resize(n + 1);
+    invfact.resize(n + 1);
     fact[0] = 1;
-
     for (ll i = 1; i <= n; ++i)
     {
         fact[i] = fact[i - 1] * i;
     }
+    invfact[n] = fact[n].inv();
+    for (ll i = n; i > 0; i--)
+    {
+        invfact[i - 1] = invfact[i] * i;
+    }
 }
 
 template <typename T1, typename T2>
-ll p(T1 n, T2 r)
+mll p(T1 n, T2 r)
 {
     if (r > n)
         return 0;
-    return fact[n] / (fact[n - r]);
+    return fact[n] * invfact[n - r];
 }
+
 template <typename T1, typename T2>
-ll c(T1 n, T2 r)
+mll c(T1 n, T2 r)
 {
+    if (r == n)
+        return 1;
     if (r > n)
         return 0;
-    return (fact[n] / fact[r]) / fact[n - r];
+    return fact[n] * invfact[n - r] * invfact[r];
 }
 
 void solve()
