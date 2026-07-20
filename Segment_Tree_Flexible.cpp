6bca5312a2b8df25882a50a05793ca2321a4beaa
Lakshya Tyagi <86817577+LakshyaTyagi15@users.noreply.github.com>
2026-07-20 22:56:53 +0530

    Refactor Segment Tree implementation with new structures

--- Segment_Tree_Flexible.cpp
+++ Segment_Tree_Flexible.cpp
@@ -1,144 +1,215 @@
 #include <bits/stdc++.h>
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-#ifdef LOCAL
-#include <debug.h>
-#else
-#define dbg(x...)
-#endif
-
 using namespace std;
-using namespace __gnu_pbds;
 
-template <typename T>
-using ordered_set =
-    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+struct Sum {
+    int64_t x;
+    static const int64_t segfactor = 0;
+    static const int64_t updatefactor = 1;
 
-using ll = long long;
-const ll mod = 1e9 + 7;
-const ll modd = 998244353;
-
-template <class Info, class Tag>
-struct SegmentTree {
-    ll n;
-    vector<tuple<Info, Tag>> seg;
-    SegmentTree(ll _n) : n(_n) { seg.resize(4 * n + 10); }
-
-    template <typename T>
-    SegmentTree(const vector<T>& a) : n((ll)a.size()) {
-        seg.resize(4 * n + 10);
-        build(0, 0, n - 1, a);
+    Sum(int64_t val = 0) {
+        x = val;
     }
 
-    template <typename T>
-    void build(ll i, ll l, ll r, const vector<T>& a) {
+    Sum operator+(const Sum& other) {
+        return x + other.x;
+    }
+
+    Sum operator*(const Sum& other) {
+        return x * other.x;
+    }
+
+    void add(int64_t d, int64_t val) {
+        x = x * updatefactor + d * val;
+    }
+};
+
+struct Max {
+    int64_t x;
+    static const int64_t segfactor = -1000000000000000000LL;
+    static const int64_t updatefactor = 0;
+
+    Max(int64_t val = segfactor) {
+        x = val;
+    }
+
+    Max operator+(const Max& other) {
+        return max(x, other.x);
+    }
+
+    Max operator*(const Max& other) {
+        return x * other.x;
+    }
+
+    void add(int64_t d, int64_t val) {
+        x = x * updatefactor + val;
+    }
+};
+
+struct Min {
+    int64_t x;
+    static const int64_t segfactor = 1000000000000000000LL;
+    static const int64_t updatefactor = 0;
+
+    Min(int64_t val = segfactor) {
+        x = val;
+    }
+
+    Min operator+(const Min& other) {
+        return min(x, other.x);
+    }
+
+    Min operator*(const Min& other) {
+        return x * other.x;
+    }
+
+    void add(int64_t d, int64_t val) {
+        x = x * updatefactor + val;
+    }
+};
+
+struct Xor {
+    int64_t x;
+    static const int64_t segfactor = 0;
+    static const int64_t updatefactor = 1;
+
+    Xor(int64_t val = 0) {
+        x = val;
+    }
+
+    Xor operator+(const Xor& other) {
+        return x ^ other.x;
+    }
+
+    Xor operator*(const Xor& other) {
+        return x * other.x;
+    }
+
+    void add(int64_t d, int64_t val) {
+        if (d & 1) {
+            x = (x * updatefactor) ^ val;
+        }
+    }
+};
+
+template<typename Info, typename Tag>
+struct SegmentTree {
+    int n;
+    vector<Info> seg;
+    vector<Tag> lazy;
+    vector<bool> has_lazy;
+
+    void pull(int i) {
+        seg[i] = seg[2 * i + 1] + seg[2 * i + 2];
+    }
+
+    template<typename T>
+    void push(int i, int l, int r, T val) {
+        seg[i].add(r - l + 1, val.x);
+
+        if (l != r) {
+            lazy[2 * i + 1].add(1, val.x);
+            lazy[2 * i + 2].add(1, val.x);
+            has_lazy[2 * i + 1] = true;
+            has_lazy[2 * i + 2] = true;
+        }
+    }
+
+    SegmentTree(int m) {
+        n = m;
+        if (n > 0) {
+            seg.resize(4 * n + 10, Info(Info::segfactor));
+            lazy.resize(4 * n + 10, Tag());
+            has_lazy.resize(4 * n + 10, false);
+        }
+    }
+
+    template<typename T>
+    SegmentTree(vector<T> a) {
+        n = a.size();
+        if (n > 0) {
+            seg.resize(4 * n + 10, Info(Info::segfactor));
+            lazy.resize(4 * n + 10, Tag());
+            has_lazy.resize(4 * n + 10, false);
+            init(a);
+        }
+    }
+
+    template<typename T>
+    void init(vector<T> &a) {
+        if (n > 0) build(0, 0, n - 1, a);
+    }
+
+    template<typename T>
+    void build(int i, int l, int r, vector<T> &a) {
         if (l == r) {
-            get<0>(seg[i]) = a[l];
+            seg[i].x = a[l];
             return;
         }
-        ll mid = (l + r) / 2;
+        int mid = l + (r - l) / 2;
         build(2 * i + 1, l, mid, a);
         build(2 * i + 2, mid + 1, r, a);
-        get<0>(seg[i]) = get<0>(seg[2 * i + 1]) + get<0>(seg[2 * i + 2]);
+        pull(i);
     }
 
-    ll query(ll x) { return query(0, 0, n - 1, x, x); }
-
-    ll query(ll x, ll y) { return query(0, 0, n - 1, x, y); }
-
-    Info query(ll i, ll l, ll r, ll x, ll y) {
-        if (get<1>(seg[i]) != 0) {
-            get<0>(seg[i]) += (r - l + 1) * get<1>(seg[i]);
-            if (l != r) {
-                get<1>(seg[2 * i + 1]) += get<1>(seg[i]);
-                get<1>(seg[2 * i + 2]) += get<1>(seg[i]);
-            }
-            get<1>(seg[i]) = 0;
+    template<typename T>
+    void update(int i, int l, int r, int x, int y, T val) {
+        if (has_lazy[i]) {
+            push(i, l, r, lazy[i]);
+            lazy[i] = Tag();
+            has_lazy[i] = false;
         }
+        if (x > r || y < l || l > r) return;
+        if (x <= l && r <= y) {
+            push(i, l, r, val);
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        update(2 * i + 1, l, mid, x, y, val);
+        update(2 * i + 2, mid + 1, r, x, y, val);
+        pull(i);
+    }
 
-        if (r < x || l > y) return 0;
-        if (l >= x && r <= y) return get<0>(seg[i]);
-        ll mid = (l + r) / 2;
+    Info query(int i, int l, int r, int x, int y) {
+        if (has_lazy[i]) {
+            push(i, l, r, lazy[i]);
+            lazy[i] = Tag();
+            has_lazy[i] = false;
+        }
+        if (x > r || y < l || l > r) {
+            return Info(Info::segfactor);
+        }
+        if (x <= l && r <= y) {
+            return seg[i];
+        }
+        int mid = l + (r - l) / 2;
         return query(2 * i + 1, l, mid, x, y) + query(2 * i + 2, mid + 1, r, x, y);
     }
 
-    template <typename T>
-    void update(ll x, ll y, T val) {
-        update(0, 0, n - 1, x, y, val);
+    template<typename T>
+    void apply(int i, T val) {
+        if (n > 0) update(0, 0, n - 1, i, i, val);
     }
-    template <typename T>
-    void update(ll idx, T val) {
-        update(0, 0, n - 1, idx, idx, val);
-    }
-    template <typename T>
-    void update(ll i, ll l, ll r, ll x, ll y, T val) {
-        if (get<1>(seg[i]) != 0) {
-            get<0>(seg[i]) += (r - l + 1) * get<1>(seg[i]);
-            if (l != r) {
-                get<1>(seg[2 * i + 1]) += get<1>(seg[i]);
-                get<1>(seg[2 * i + 2]) += get<1>(seg[i]);
-            }
-            get<1>(seg[i]) = 0;
-        }
 
-        if (r < x || l > y) return;
-        if (l >= x && r <= y) {
-            get<0>(seg[i]) += (r - l + 1) * val;
-            if (l != r) {
-                get<1>(seg[2 * i + 1]) += val;
-                get<1>(seg[2 * i + 2]) += val;
-            }
-            return;
-        }
-        ll mid = (l + r) / 2;
-        update(2 * i + 1, l, mid, x, y, val);
-        update(2 * i + 2, mid + 1, r, x, y, val);
-        get<0>(seg[i]) = get<0>(seg[2 * i + 1]) + get<0>(seg[2 * i + 2]);
+    template<typename T>
+    void apply(int l, int r, T val) {
+        if (n > 0) update(0, 0, n - 1, l, r, val);
+    }
+
+    auto get(int i) {
+        return query(0, 0, n - 1, i, i).x;
+    }
+
+    auto get(int l, int r) {
+        return query(0, 0, n - 1, l, r).x;
     }
 };
 
-template <typename T>
-struct Info {
-    T val = 0;  // if max, min is being used intialize val = -1e9 or val = 1e9 for
-    // handling all types of query
-    Info(T v = 0) : val(v) {}
-    operator T() const { return val; }
-};
+void solve() {
 
-template <typename T>
-Info<T> operator+(const Info<T>& a, const Info<T>& b) {
-    return Info<T>(a.val + b.val);  // Use max, min, gcd, lcm, etc as per
-    // requirement like max(a.val, b.val)
-}
-template <typename T>
-Info<T> operator+=(Info<T>& a, const Info<T>& b) {
-    a.val = a.val + b.val;  // If we have to change value then a.val = b.val else
-    // handle like + operator;
-    return a;
 }
 
-template <typename T>
-struct Tag {
-    T val = 0;
-    Tag(T v = 0) : val(v) {}
-    operator T() const { return val; }
-};
-
-template <typename T>
-Tag<T> operator+=(Tag<T>& a, const Tag<T>& b) {
-    a.val = a.val + b.val;  // If we have to change value then a.val = b.val else
-    // handle like + operator of Info;
-    return a;
-}
-
-template <typename T>
-SegmentTree(const vector<T>&) -> SegmentTree<Info<T>, Tag<T>>;
-
-void solve() {}
-
-int main() {
+int32_t main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -147,7 +218,6 @@ int main() {
     cin >> t;
 
     for (int i = 1; i <= t; i++) {
-        cerr << "Testcase No. - " << i << "\n";
         solve();
     }
 
